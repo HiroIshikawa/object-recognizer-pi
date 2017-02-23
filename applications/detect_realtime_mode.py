429f3093feefdca33c8d57691e5d8027ae6e8e3a
HiroIshikawa <hiro52ishikawa@gmail.com>
2017-02-22 18:26:39 -0800

    Refact mode controller prototype for webcam

--- applications/detect_realtime_mode.py
+++ applications/detect_realtime_mode.py
@@ -2,8 +2,6 @@ from detect import *
 from interface import *
 from measure import *
 from preprocess import *
-#opens up a webcam feed so you can then test your classifer in real time
-#using detectMultiScale
 from sys import argv
 import sys
 import cv2
@@ -17,9 +15,9 @@ def mean(l):
         return 0.0
     return sum(l)/len(l)
 
-def navigate(stop_event):
+def check_candidates(stop_event):
     global candidates
-    t = threading.Timer(0.5, navigate, args=[stop_event])
+    t = threading.Timer(0.5, check_candidates, args=[stop_event])
     t.start()
     if stop_event.is_set():
         t.cancel()
@@ -59,18 +57,14 @@ def detection_system(cap,cas_params):
     global track_flag
     global avg_pos
     global candidates
-    # track_flag = False
     avg_pos = 0.
     print("In auto..")
-    # navigate(stop_event)
     while(True):
         ret, raw_img = cap.read()
         imshape = raw_img.shape
         vertices = np.array([[(0,imshape[0]),(0,int(imshape[0]/2)), 
             (int(imshape[1]),int(imshape[0]/2)), (imshape[1],imshape[0])]], dtype=np.int32)
-        # cv2.imshow("Raw", img)
         processed_img = region_of_interest(raw_img, vertices)
-        # cv2.imshow("Preprocessed", img)
         rects, detected_img = detect(processed_img, cas_params)
         img = box(rects, detected_img)
         measure(img, rects, candidates)
@@ -80,36 +74,38 @@ def auto(stop_event):
     track_flag = False
     global avg_pos
     global candidates
-    navigate(stop_event)
+    check_candidates(stop_event)
     while(not stop_event.is_set()):
         if track_flag:
             track(avg_pos)
             track_flag = False
             time.sleep(.1)
 
-def mode_controller(power, cap, cas_params):
+def mode_controller(cap, cas_params):
+    global power
     while (True):
-        input = raw_input("Manual (m) or Auto (a)?: ")
+        input = raw_input("Manual (m), Auto (a), or Quit (q)?: ")
         if input == 'm':
-            print('Activating manual mode..')
+            print('Activating manual mode...')
             manual()
         elif input == 'a':
-            print('Activating auto mode..')
+            print('Activating auto mode...')
             auto_thread_stop = threading.Event()
             auto_thread = threading.Thread(target=auto, args=[auto_thread_stop])
             auto_thread.daemon = True
             auto_thread.start()
-            input = raw_input("Mode Change Accepting...")
+            input = raw_input("Mode Change Accepting (m)...")
             if input:
                 auto_thread_stop.set()
                 print("Object Tracker Stopped.")
                 pass
         elif input == 'q':
-            print('Deactivating..')
+            print('Powering Off...')
             power = 'off'
         else:
             pass
 
+# Params for camera and object detection
 scale_factor = float(argv[1])
 min_neighs = int(argv[2])
 obj_w = int(argv[3])
@@ -117,36 +113,38 @@ obj_h = int(argv[4])
 win_w = int(argv[5])
 win_h = int(argv[6])
 
-# pack parms for cascades
-cas_params = (scale_factor, min_neighs, obj_w, obj_h)
-
-# global cap
+# Setting up camera
 cap = cv2.VideoCapture(0)
 cap.set(3,win_w)
 cap.set(4,win_h)
 time.sleep(.1)
 
-# global img
+# Pack parms for cascades
+cas_params = (scale_factor, min_neighs, obj_w, obj_h)
+
+# Image file used accross threads
 global img
 ret, img = cap.read()
 
-# power
+# Power on/off
+global power
 power = 'on'
 
-
+# Candidates are list of detected objects which has max size in a frame and
 global candidates
 candidates = []
+
+# Initiate a thread for detection system
 detection_thread = threading.Thread(target=detection_system, args=[cap, cas_params])
 detection_thread.daemon = True
 detection_thread.start()
-print("Detection System Initiated.")
 
-# now threading1 runs regardless of user input
-controller_thread = threading.Thread(target=mode_controller, args=[power,cap,cas_params])
+# Initiate a therad for control system
+controller_thread = threading.Thread(target=mode_controller, args=[cap,cas_params])
 controller_thread.daemon = True
 controller_thread.start()
-print("Control System Initiated.")
 
+# Main thread, exit when user send quit command in control system prompt
 while(power):
     if img.any():
         window_name="Cascaded"
@@ -154,5 +152,4 @@ while(power):
         cv2.imshow(window_name, img)
         cv2.waitKey(1)
     if power=='off':
-        "Powering off..."
         break
